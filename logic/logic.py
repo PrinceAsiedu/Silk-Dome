a4fb1a80fe585a76f7e483ae37df7cf07068f113
Prince Asiedu <prince14asiedu@gmail.com>
2022-04-04 02:43:14 +0000

    driver update

---
+++ logic/logic.py
@@ -0,0 +1,61 @@
+# Date: 02-04-2022
+# Author: Prince Oforh Asiedu
+# Description:  Automated Intrusion Detection and Defence System 
+# capable of initiating counter-measures on system breach.
+
+class Driver:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def restart(self):
+        pass
+
+    def status(self):
+        pass
+
+    def get_log(self):
+        pass
+
+    def get_config(self):
+        pass
+
+    def set_config(self):
+        pass
+
+    def get_alerts(self):
+        pass
+
+    def get_alert_log(self):
+        pass
+
+    def get_alert_config(self):
+        pass
+
+    def set_alert_config(self):
+        pass
+
+    def get_alert_status(self):
+        pass
+
+    def get_alert_history(self):
+        pass
+
+    def get_alert_history_log(self):
+        pass
+
+    def get_alert_history_config(self):
+        pass
+
+    def set_alert_history_config(self):
+        pass
+
+    def get_alert_history_status(self):
+        pass
+
+class Alert:
---
+++ run.bat
@@ -0,0 +1,1 @@
+py logic\driver.py 
